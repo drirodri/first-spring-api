689815a59c33068989277730492402c6496b6499
Adriano <adriano.rsilva97@gmail.com>
2025-03-22 19:49:14 -0300

    cleanup: removed unnecessary lines

--- src/main/java/com/dri/first_api/Service/UserServiceImpl.java
+++ src/main/java/com/dri/first_api/Service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.dri.first_api.Service;
 
 import com.dri.first_api.Entity.User;
-import com.dri.first_api.Model.UserModel;
 import com.dri.first_api.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +25,7 @@ public class UserServiceImpl implements UserService {
         if (user.isEmpty()) {
             throw new RuntimeException("User with id " + id + " not found");
         }
-        
+
         return user;
     }
 
